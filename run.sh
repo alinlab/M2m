19f42dc776bb057a04f16ca1a2ad67601ed6ae26
bbuing9 <wogudehowl@kaist.ac.kr>
2020-12-23 18:17:12 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -31,5 +31,5 @@ CUDA_VISIBLE_DEVICES=0 python train.py --no_over --loss_type Focal --focal_gamma
 CUDA_VISIBLE_DEVICES=0 python train.py --no_over -c --loss_type LDAM --eff_beta 0.999 --ratio 100 --decay 2e-4 --model resnet32_norm --dataset cifar10 --lr 0.1 --batch-size 128 --name 'LDAM-DRW' --warm 160 --epoch 200  
 
 # Script for running our method (M2m) 
-CUDA_VISIBLE_DEVICES=0 python train.py -gen --ratio 100 --decay 2e-4 --model resnet32 --dataset cifar10 --lr 0.1 --batch-size 128 --name 'M2m' --beta 0.999 --lam 0.5 --gamma 0.9 --step_size 0.1 --attack_iter 10 --warm 160 --epoch 200 --net_g ./checkpoint/erm_r100_c10_trial1.t7 
+CUDA_VISIBLE_DEVICES=0 python train.py -gen -r --ratio 100 --decay 2e-4 --model resnet32 --dataset cifar10 --lr 0.1 --batch-size 128 --name 'M2m' --beta 0.999 --lam 0.5 --gamma 0.9 --step_size 0.1 --attack_iter 10 --warm 160 --epoch 200 --net_g ./checkpoint/erm_r100_c10_trial1.t7 
 
