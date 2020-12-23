3bea0cd4799da3218036c178a3fda5314a482477
bbuing9 <wogudehowl@kaist.ac.kr>
2020-12-23 18:16:26 +0900

    add resume option

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@
 
 # SMOTE
 
-CUDA_VISIBLE_DEVICES=0 python train.py -s --ratio 100 --decay 2e-4 --model resnet32 --dataset cifar10 --lr 0.1 --batch-size 128 --name 'SMOTE' --warm 0 --epoch 200  
+#CUDA_VISIBLE_DEVICES=0 python train.py -s --ratio 100 --decay 2e-4 --model resnet32 --dataset cifar10 --lr 0.1 --batch-size 128 --name 'SMOTE' --warm 0 --epoch 200  
 
 # Script for running re-weighting (RW)
 
@@ -31,5 +31,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py -s --ratio 100 --decay 2e-4 --model resne
 #CUDA_VISIBLE_DEVICES=0 python train.py --no_over -c --loss_type LDAM --eff_beta 0.999 --ratio 100 --decay 2e-4 --model resnet32_norm --dataset cifar10 --lr 0.1 --batch-size 128 --name 'LDAM-DRW' --warm 160 --epoch 200  
 
 # Script for running our method (M2m) 
-#CUDA_VISIBLE_DEVICES=0 python train.py -r -gen --ratio 100 --decay 2e-4 --model resnet32 --dataset cifar10 --lr 0.1 --batch-size 128 --name 'M2m' --beta 0.999 --lam 0.5 --gamma 0.9 --step_size 0.1 --attack_iter 10 --warm 160 --epoch 200 --net_t ./checkpoint/ckpt_erm_epoch159_919.t7 --net_g ./checkpoint/erm_r100_c10_trial1.t7 
+CUDA_VISIBLE_DEVICES=0 python train.py -r -gen --ratio 100 --decay 2e-4 --model resnet32 --dataset cifar10 --lr 0.1 --batch-size 128 --name 'M2m' --beta 0.999 --lam 0.5 --gamma 0.9 --step_size 0.1 --attack_iter 10 --warm 160 --epoch 200 --net_t ./checkpoint/ckpt_erm_epoch159_919.t7 --net_g ./checkpoint/erm_r100_c10_trial1.t7 
+
+
 
